16d78dafaf8d103256893ca50547ed15b47a3ff7
Hyunseung Cho <cho6646kr@hotmail.com>
2020-06-14 00:47:43 +0900

    fixed a bit

--- app/src/main/java/com/jc_lab/mask_finder/MainActivity.kt
+++ app/src/main/java/com/jc_lab/mask_finder/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.jc_lab.mask_finder
 
 import android.graphics.Color.*
-import android.icu.text.IDNA
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import android.view.View
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageButton
@@ -24,10 +22,8 @@ import com.naver.maps.map.overlay.*
 import com.naver.maps.map.util.FusedLocationSource
 import com.naver.maps.map.util.MarkerIcons
 import org.json.JSONObject
-import java.util.concurrent.Executor
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
 import kotlin.math.floor
 import kotlin.math.max
 import kotlin.math.min
@@ -37,8 +33,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var queue: RequestQueue
     private lateinit var locationSource: FusedLocationSource
     private lateinit var naverMap: NaverMap
-    private var mLat: Double = 0.0
-    private var mLng: Double = 0.0
+    private lateinit var gpsButton: ImageButton
+    private lateinit var searchButton: Button
+
     private var mMeter: Int = 500
     private var currentLat: Double = 0.0
     private var currentLng: Double = 0.0
@@ -47,14 +44,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private var maskInfoCollected: Boolean = false
     private var selectedInfoWindow: InfoWindow = InfoWindow()
     private var storeDatas = mutableListOf<StoreData>()
-    private lateinit var gpsButton: ImageButton
-    private lateinit var searchButton: Button
     private var markers = mutableListOf<Marker>()
     private var infoWindows = mutableListOf<InfoWindow>()
-
-//    private lateinit var postOffice: OverlayImage
-//    private lateinit var nongHyup: OverlayImage
-//    private lateinit var drugStore: OverlayImage
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,14 +59,62 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             fm.beginTransaction().add(R.id.map, it).commit()
         }
 
-        gpsButton = findViewById<ImageButton>(R.id.gps_button)
-        searchButton = findViewById<Button>(R.id.search_button)
+        gpsButton = findViewById(R.id.gps_button)
+        searchButton = findViewById(R.id.search_button)
 
         queue = Volley.newRequestQueue(this)
 
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
 
         mapFragment.getMapAsync(this)
+    }
+
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+
+        naverMap.addOnLocationChangeListener { location ->
+            currentLat = location.latitude
+            currentLng = location.longitude
+            currentBearing = location.bearing
+            if( gpsFollow )
+            {
+                val cameraUpdate = scrollTo(LatLng(location.latitude, location.longitude))
+                naverMap.moveCamera(cameraUpdate)
+                if( !maskInfoCollected )
+                {
+                    getMaskInfo(currentLat, currentLng, mMeter)
+                    maskInfoCollected = true
+                }
+            }
+        }
+
+        naverMap.addOnCameraChangeListener { reason, animated ->
+            if(reason == REASON_GESTURE) gpsFollow = false
+        }
+
+        naverMap.addOnCameraIdleListener {
+            mMeter = zoomSet(naverMap.cameraPosition.zoom).toInt()
+        }
+
+        naverMap.setOnMapClickListener { point, coord ->
+            selectedInfoWindow.close()
+        }
+
+        gpsButton.setOnClickListener{ view ->
+            var cameraUpdate = toCameraPosition(CameraPosition(LatLng(currentLat, currentLng), 14.0)).animate(CameraAnimation.Easing)
+            naverMap.moveCamera(cameraUpdate)
+            gpsFollow = true
+            maskInfoCollected = false
+        }
+
+        searchButton.setOnClickListener{ view ->
+            markerAndInfoWindowClear()
+            val cameraPositionTarget = naverMap.cameraPosition.target
+            getMaskInfo(cameraPositionTarget.latitude, cameraPositionTarget.longitude, mMeter)
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
@@ -93,16 +132,13 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun getMaskInfo(lat: Double?, lng: Double?, meter: Int){
-        if(lat == null || lng == null)
-            return
-        if(lat < 33.0 || lat >43.0)
-            return
-        if(lng < 124.0 || lng > 132.0)
-            return
-        if(meter <= 0 || meter > 5000)
-            return
+        if(lat == null || lng == null) return
+        if(lat < 33.0 || lat > 43.0) return
+        if(lng < 124.0 || lng > 132.0) return
+        if(meter <= 0 || meter > 5000) return
 
         val url = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat=${lat}&lng=${lng}&m=${meter}"
+
         // Request a string response from the provided URL.
         val stringRequest = VolleyUTF8EncodingStringRequest( Request.Method.GET, url,
             Response.Listener<String> { response ->
@@ -113,19 +149,19 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 for(i in 0 until cnt)
                 {
                     val item = stores.getJSONObject(i)
-                    val itemCode: String = item.getString("code")
-                    val itemAddr: String? = item.getString("addr")
-                    val itemCreatedAt: String? = item.getString("created_at")
-                    val itemLat: String = item.getString("lat")
-                    val itemLng: String = item.getString("lng")
-                    val itemName: String = item.getString("name")
-                    val itemRemainStat: String? = item.getString("remain_stat")
-                    val itemStockAt: String? = item.getString("stock_at")
-                    val itemType: String? = item.getString("type")
 
-                    storeDatas.add(StoreData(itemCode, itemAddr, itemCreatedAt, itemLat.toDouble(), itemLng.toDouble(), itemName, itemRemainStat, itemStockAt, itemType))
+                    storeDatas.add(StoreData(item.getString("code"),
+                        item.getString("addr"),
+                        item.getString("created_at"),
+                        item.getString("lat").toDouble(),
+                        item.getString("lng").toDouble(),
+                        item.getString("name"),
+                        item.getString("remain_stat"),
+                        item.getString("stock_at"),
+                        item.getString("type")))
                 }
                 markerAndInfoWindowSetup(cnt)
+
                 Toast.makeText(this, "현재 위치, 경도: ${floor(lat*100)/100}, 위도: ${floor(lng*100)/100}에서 $cnt 곳의 마스크 판매소를 찾았습니다", Toast.LENGTH_LONG).show()
             },
             Response.ErrorListener { Log.d("error","That didn't work!") })
@@ -145,51 +181,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             for(j in 0 until mCnt)
             {
                 val storeDataItem = storeDatas.get(j)
-                val marker = Marker().apply{
-                    position = LatLng(storeDataItem.lat, storeDataItem.lng)
-                    icon = MarkerIcons.BLACK
-                    width = 100
-                    height = 130
-                    captionText = storeDataItem.name
-                    isHideCollidedSymbols = true
-                    isHideCollidedCaptions = true
-                    isHideCollidedMarkers = true
+                val marker = putInfoInMarker(storeDataItem)
 
-//                    captionAligns = arrayOf(Align.Right)
-                    if(storeDataItem.remain_stat == "plenty")
-                    {
-                        iconTintColor = GREEN
-                        zIndex = 100
-                    }
-                    else if(storeDataItem.remain_stat == "some")
-                    {
-                        iconTintColor = YELLOW
-                        zIndex = 0
-                    }
-                    else if(storeDataItem.remain_stat == "few")
-                    {
-                        iconTintColor = RED
-                        zIndex = -10
-                    }
-                    else
-                    {
-                        iconTintColor = GRAY
-                        zIndex = -100
-                    }
-                    if(storeDataItem.type=="01")
-                    {
-                        type = "약국"
-                    }
-                    else if(storeDataItem.type == "02")
-                    {
-                        type = "농협 하나로 마트"
-                    }
-                    else
-                    {
-                        type = "우체국"
-                    }
+                if(storeDataItem.type=="01") type = "약국"
+                else if(storeDataItem.type == "02") type = "농협 하나로 마트"
+                else type = "우체국"
 
-                }
                 markers.add(marker)
                 val infoWindow = InfoWindow()
                 infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(this) {
@@ -223,7 +220,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
 
         executor.shutdown()
-//        executor.awaitTermination(1, TimeUnit.SECONDS)
     }
 
     private fun markerAndInfoWindowClear(){
@@ -235,65 +231,46 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         infoWindows.clear()
     }
 
-    @UiThread
-    override fun onMapReady(naverMap: NaverMap) {
-        this.naverMap = naverMap
-//        val options = NaverMapOptions()
-//            .camera(CameraPosition(LatLng(35.1798159, 129.0750222), 8.0))
-//            .mapType(NaverMap.MapType.Terrain)
-        // current location of camera
-//        var cameraPosition = naverMap.cameraPosition
-        naverMap.locationSource = locationSource
-        naverMap.locationTrackingMode = LocationTrackingMode.Follow
-        val locationOverlay = naverMap.locationOverlay
+    private fun putInfoInMarker(storeDataItem: StoreData) : Marker
+    {
+        return Marker().apply {
+            position = LatLng(storeDataItem.lat, storeDataItem.lng)
+            icon = MarkerIcons.BLACK
+            width = 100
+            height = 130
+            captionText = storeDataItem.name
+            isHideCollidedSymbols = true
+            isHideCollidedCaptions = true
+            isHideCollidedMarkers = true
 
-        naverMap.addOnLocationChangeListener { location ->
-            currentLat = location.latitude
-            currentLng = location.longitude
-            currentBearing = location.bearing
-            if( gpsFollow )
+            if(storeDataItem.remain_stat == "plenty")
             {
-                val cameraUpdate = scrollTo(LatLng(location.latitude, location.longitude))
-                naverMap.moveCamera(cameraUpdate)
-//                locationOverlay.bearing = currentBearing
-                if( !maskInfoCollected )
-                {
-                    getMaskInfo(currentLat, currentLng, mMeter)
-                    maskInfoCollected = true
-                }
+                iconTintColor = GREEN
+                zIndex = 100
+            }
+            else if(storeDataItem.remain_stat == "some")
+            {
+                iconTintColor = YELLOW
+                zIndex = 0
+            }
+            else if(storeDataItem.remain_stat == "few")
+            {
+                iconTintColor = RED
+                zIndex = -10
+            }
+            else
+            {
+                iconTintColor = GRAY
+                zIndex = -100
             }
         }
+    }
 
-        naverMap.addOnCameraChangeListener { reason, animated ->
-            if(reason == REASON_GESTURE)
-            {
-                gpsFollow = false
-            }
-        }
-
-        naverMap.addOnCameraIdleListener {
-            var zoom = naverMap.cameraPosition.zoom
-            zoom = min(zoom, 14.0)
-            zoom = max(zoom, 9.0)
-            mMeter = (500 + 4500*(14-zoom)/5).toInt()
-        }
-
-        naverMap.setOnMapClickListener { point, coord ->
-            selectedInfoWindow.close()
-        }
-
-        gpsButton.setOnClickListener{ view ->
-            var cameraUpdate = toCameraPosition(CameraPosition(LatLng(currentLat, currentLng), 14.0)).animate(CameraAnimation.Easing)
-            naverMap.moveCamera(cameraUpdate)
-            gpsFollow = true
-            maskInfoCollected = false
-        }
-
-        searchButton.setOnClickListener{ view ->
-            markerAndInfoWindowClear()
-            val cameraPositionTarget = naverMap.cameraPosition.target
-            getMaskInfo(cameraPositionTarget.latitude, cameraPositionTarget.longitude, mMeter)
-        }
+    private fun zoomSet(zoom : Double) : Double
+    {
+        var zoomToChange = min(zoom, 14.0)
+        zoomToChange = max(zoomToChange, 9.0)
+        return 500 + 4500*(14-zoomToChange)/5
     }
 
     companion object{
--- app/src/main/java/com/jc_lab/mask_finder/StoreData.kt
+++ app/src/main/java/com/jc_lab/mask_finder/StoreData.kt
@@ -1,5 +1,3 @@
 package com.jc_lab.mask_finder
 
-import org.json.JSONObject
-
 data class StoreData(val code: String, val addr: String?, val createdAt: String?, val lat: Double, val lng: Double, val name: String, val remain_stat: String?, val stockAt: String?, val type: String?)
